0b4c114c6e756bba2fcd53ab77325428372c9a57
kauanatomb <kauanatomb@gmail.com>
2025-08-26 10:00:20 +0200

    improve fle identation for norm

--- src/expandvar/main_expand.c
+++ src/expandvar/main_expand.c
@@ -6,134 +6,129 @@
 /*   By: ktombola <ktombola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 10:32:28 by ktombola          #+#    #+#             */
-/*   Updated: 2025/08/12 10:55:19 by ktombola         ###   ########.fr       */
+/*   Updated: 2025/08/26 08:43:04 by ktombola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char    *extract_key(char *p)
+char	*extract_key(char *p)
 {
-    int     i;
+	int	i;
 
-    i = 0;
-    if (!ft_isalpha(p[0]) && p[0] != '_')
-        return (NULL);
-    while ((p[i] && ft_isalnum(p[i])) || p[i] == '_')
-        i++;
-    return (ft_substr(p, 0, i));
+	i = 0;
+	if (!ft_isalpha(p[0]) && p[0] != '_')
+		return (NULL);
+	while (p[i] && (ft_isalnum(p[i]) || p[i] == '_'))
+		i++;
+	return (ft_substr(p, 0, i));
 }
 
-char    *getenv_from_envp(const char *key, char **env)
+char	*getenv_from_envp(const char *key, char **env)
 {
-    int     i;
-    int     len;
+	int	i;
+	int	len;
 
-    len = ft_strlen(key);
-    i = 0;
-    while (env[i])
-    {
-        if (ft_strncmp(env[i], key, len) == 0 && env[i][len] == '=')
-            return (env[i] + len + 1);
-        i++;
-    }
-    return (NULL);
+	len = ft_strlen(key);
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], key, len) == 0 && env[i][len] == '=')
+			return (env[i] + len + 1);
+		i++;
+	}
+	return (NULL);
 }
 
-char *expand_var(char *str, t_shell *shell)
+char	*expand_var(char *str, t_shell *shell)
 {
-    char    *result;
-    char    *p;
-    char    *key;
-    char    *value;
-    char    tmp[2];
+	char	*result;
+	char	*p;
+	char	*key;
+	char	*value;
+	char	tmp[2];
 
-    result = ft_strdup("");
-    if (!result)
-        return (NULL);
-    p = str;
-    while (*p)
-    {
-        if (*p == '$')
-        {
-            p++;
-            // special case: $?
-            if (*p == '?')
-            {
-                value = ft_itoa(shell->error); // to check status
-                result = ft_strjoin_and_free(result, value);
-                p++;
-                continue;
-            }
-            // extract key (ex: PATH, HOME)
-            key = extract_key(p);
-            if (key)
-            {
-                value = getenv_from_envp(key, shell->env);
-                if (value)
-                    result = ft_strjoin_and_free(result, value);
-                p += ft_strlen(key);
-                free(key);
-            }
-            else
-            {
-                // copy literal
-                p++;
+	result = ft_strdup("");
+	if (!result)
+		return (NULL);
+	p = str;
+	while (*p)
+	{
+		if (*p == '$')
+		{
+			p++;
+			if (*p == '?')
+			{
+				value = ft_itoa(shell->error);
+				result = ft_strjoin_and_free(result, value);
+				p++;
+				continue ;
+			}
+			key = extract_key(p);
+			if (key)
+			{
+				value = getenv_from_envp(key, shell->env);
+				if (value)
+					result = ft_strjoin_and_free(result, value);
+				p += ft_strlen(key);
+				free(key);
+			}
+			else
+			{
                 tmp[0] = *p;
-                tmp[1] = '\0';
-                result = ft_strjoin_and_free(result, tmp);
-                p++;
-            }
-        }
-        else
-        {
-            // copy literal
-            tmp[0] = *p;
-            tmp[1] = '\0';
-            result = ft_strjoin_and_free(result, tmp);
-            p++;
-        }
-    }
-    return (result);
+				tmp[1] = '\0';
+				result = ft_strjoin_and_free(result, tmp);
+				p++;
+			}
+		}
+		else
+		{
+			tmp[0] = *p;
+			tmp[1] = '\0';
+			result = ft_strjoin_and_free(result, tmp);
+			p++;
+		}
+	}
+	return (result);
 }
 
-int main_expand(t_shell *shell)
+int	main_expand(t_shell *shell)
 {
-    t_cmd   *cmd;
-    int     tok_i;
-    int     i;
-    char    *expanded;
-    t_redir *r;
+	t_cmd	*cmd;
+	int		tok_i;
+	int		i;
+	char	*expanded;
+	t_redir	*r;
 
-    cmd = shell->cmds;
-    tok_i = 0;
-    while (cmd)
-    {
-        i = 0;
-        while (cmd->argv[i])
-        {
-            if (shell->tokens[tok_i].quote_type != SINGLE)
-            {
-                expanded = expand_var(cmd->argv[i], shell);
-                free(cmd->argv[i]);
-                cmd->argv[i] = expanded;
-            }
-            i++;
-            tok_i++;
-        }
-        r = cmd->redirs;
-        while (r)
-        {
-            if (r->type != HEREDOC && r->filename)
-            {
-                expanded = expand_var(r->filename, shell);
-                free(r->filename);
-                r->filename = expanded;
-            }
-            r = r->next;
-            tok_i++;
-        }
-        cmd = cmd->next;
-    }
-    return (ERR_NONE);
+	cmd = shell->cmds;
+	tok_i = 0;
+	while (cmd)
+	{
+		i = 0;
+		while (cmd->argv[i])
+		{
+			if (shell->tokens[tok_i].quote_type != SINGLE)
+			{
+				expanded = expand_var(cmd->argv[i], shell);
+				free(cmd->argv[i]);
+				cmd->argv[i] = expanded;
+			}
+			i++;
+			tok_i++;
+		}
+		r = cmd->redirs;
+		while (r)
+		{
+			if (r->type != HEREDOC && r->filename)
+			{
+				expanded = expand_var(r->filename, shell);
+				free(r->filename);
+				r->filename = expanded;
+			}
+			r = r->next;
+			tok_i++;
+		}
+		cmd = cmd->next;
+	}
+	return (ERR_NONE);
 }
